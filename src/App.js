42edee0db771e028a33be85b3fb412cdb493595b
Roman Suvorkov <suvorkovrn@gmail.com>
2020-10-07 22:35:51 +0500

    Added how to page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import InfoCards from './components/InfoCards';
 import Slider from './components/Slider';
+import HowTo from './components/HowTo';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <InfoCards />
       <Slider />
+      <HowTo />
     </>
   );
 }
